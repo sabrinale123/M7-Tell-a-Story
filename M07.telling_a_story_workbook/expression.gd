5243c7bf1b4c9bd8766a735556aef6a52469f1cd
sabrinale123 <sale2@s.sfusd.edu>
2025-11-07 14:03:57 -0800

    M7 L8

--- M07.telling_a_story_workbook/expression.gd
+++ M07.telling_a_story_workbook/expression.gd
@@ -1,1 +1,16 @@
 extends Control
+
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
+
+
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+}
